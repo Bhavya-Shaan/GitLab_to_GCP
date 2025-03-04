1484a8a55ad9834b053a31703f5b10384b4b1d07
Bhavya-Shaan <55462872+Bhavya-Shaan@users.noreply.github.com>
2025-03-04 16:15:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+#variable "do_token" {}
+#variable "domain_name" {}
+
+
+variable "project" {}
+variable "location" {}
+variable "zone" {}
+
+#variable "project" = "gcp-project"
+#variable "region" = "us-central1"
+#variable "zone" = "us-central1-b"
